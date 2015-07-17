d114a7bcc53501574e24bf922d486677fe939831
Matt Ward <matt@matthewward.net>
2015-07-17 15:57:36 -0700

    Adding in file upload endpoint

---
+++ src/main/java/org/lgb/Application.java
@@ -0,0 +1,21 @@
+package org.lgb;
+
+import org.glassfish.jersey.filter.LoggingFilter;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.lgb.web.FileResource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Application extends javax.ws.rs.core.Application {
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		final Set<Class<?>> classes = new HashSet<Class<?>>();
+		// register resources and features
+		classes.add(MultiPartFeature.class);
+		classes.add(FileResource.class);
+		classes.add(LoggingFilter.class);
+		return classes;
+	}
+}
--- src/main/java/org/lgb/web/FileResource.java
+++ src/main/java/org/lgb/web/FileResource.java
@@ -1,10 +1,14 @@
 package org.lgb.web;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import io.swagger.annotations.*;
 import org.apache.log4j.Logger;
 import org.lgb.model.User;
@@ -12,12 +16,17 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.lgb.util.Hibernate;
+import org.glassfish.jersey.media.multipart.FormDataParam;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 @Path("file")
 @Api(value = "file")
-@Produces(MediaType.APPLICATION_JSON)
 public class FileResource {
-
 	
 	protected static Logger logger = Logger.getLogger("service");
 	protected static SessionFactory sessionFactory = Hibernate.getSessionFactory();
@@ -30,8 +39,9 @@ public class FileResource {
      */
     @GET
 	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
 	@ApiOperation(value = "Find a file by id")
-    public User getIt(@PathParam("id") String id) {
+    public User getFile(@PathParam("id") String id) {
 		Session session = sessionFactory.getCurrentSession();
 		Transaction trans = session.beginTransaction();
 		User user = (User) session.get(User.class, id);
@@ -39,4 +49,20 @@ public class FileResource {
 
         return user;
     }
+	
+	@POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(@FormDataParam("file") InputStream uploadedInputStream, @FormDataParam("file") FormDataContentDisposition fileDetail) {
+ 
+		//String uploadedFileLocation = "d://uploaded/" + fileDetail.getFileName();
+ 
+		// save it
+		//writeToFile(uploadedInputStream, uploadedFileLocation);
+ 
+		//String output = "File uploaded to : " + uploadedFileLocation;
+ 
+		return Response.status(200).entity(1).build();
+ 
+	}
 }
